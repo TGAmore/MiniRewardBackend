ccdb4df4799a8443553f11077cc2bf610a6bd675
TGAmore <98864996+TGAmore@users.noreply.github.com>
2025-09-17 13:56:53 +0300

    Add files via upload

---
+++ balance.js
@@ -0,0 +1,10 @@
+const usersBalance = require('./db');
+
+export default function handler(req, res) {
+  const { userId } = req.query;
+  if (!usersBalance[userId]) {
+    usersBalance[userId] = { points: 0, invites: 0, log: [], withdrawRequests: [] };
+  }
+  const user = usersBalance[userId];
+  res.status(200).json({ user_id: userId, balance: user.points, today_log: user.log, invites: user.invites });
+}
---
+++ referral.js
@@ -0,0 +1,8 @@
+const usersReferral = require('./db');
+
+export default function handler(req, res) {
+  const { userId } = req.query;
+  if (!usersReferral[userId]) usersReferral[userId] = { points: 0, invites: 0, log: [], withdrawRequests: [] };
+  const user = usersReferral[userId];
+  res.status(200).json({ user_id: userId, invites: user.invites });
+}
